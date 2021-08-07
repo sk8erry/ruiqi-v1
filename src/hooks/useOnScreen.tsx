6ae5b70515d55e84fb0704872d04fdd1e347b173
Ruiqi Yang <grandruiqi@hotmail.com>
2021-08-07 13:36:12 -0700

    disable useOnScreen

--- src/hooks/useOnScreen.tsx
+++ src/hooks/useOnScreen.tsx
@@ -3,15 +3,15 @@ import * as React from 'react'
 export const useOnScreen = (ref: any) => {
   const [isInView, setIsInView] = React.useState(false)
 
-  const observer = new IntersectionObserver(([entry]) => setIsInView(entry.isIntersecting))
+  // const observer = new IntersectionObserver(([entry]) => setIsInView(entry.isIntersecting))
 
-  React.useEffect(() => {
-    observer.observe(ref.current)
-    // Remove the observer as soon as the component is unmounted
-    return () => {
-      observer.disconnect()
-    }
-  }, [])
+  // React.useEffect(() => {
+  //   observer.observe(ref.current)
+  //   // Remove the observer as soon as the component is unmounted
+  //   return () => {
+  //     observer.disconnect()
+  //   }
+  // }, [])
 
   return isInView
 }
