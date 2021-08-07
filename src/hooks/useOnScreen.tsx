fab987d57e02bb49b7ced7b3c55f46a2c65992f6
Ruiqi Yang <grandruiqi@hotmail.com>
2021-08-07 13:39:09 -0700

    update useOnScreen logic

--- src/hooks/useOnScreen.tsx
+++ src/hooks/useOnScreen.tsx
@@ -3,15 +3,14 @@ import * as React from 'react'
 export const useOnScreen = (ref: any) => {
   const [isInView, setIsInView] = React.useState(false)
 
-  // const observer = new IntersectionObserver(([entry]) => setIsInView(entry.isIntersecting))
-
-  // React.useEffect(() => {
-  //   observer.observe(ref.current)
-  //   // Remove the observer as soon as the component is unmounted
-  //   return () => {
-  //     observer.disconnect()
-  //   }
-  // }, [])
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => setIsInView(entry.isIntersecting))
+    observer.observe(ref.current)
+    // Remove the observer as soon as the component is unmounted
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
 
   return isInView
 }
